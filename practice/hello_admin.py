c1c59b5f53a51fb9a59a501c9d269ee2bea04208
Syrgak-2022 <syrgakimankulov@gmail.com>
2023-02-07 14:38:44 +0300

    add new tasks in names

--- practice/hello_admin.py
+++ practice/hello_admin.py
@@ -7,13 +7,25 @@
 
 
 
-cars = ['bmw', 'audi', 'kia', 'lada', 'haval']
-for cars in cars:
-    if cars == 'bmw':
-        print(f"Hello, {cars.upper()}, you look very cool!")
-    elif cars == 'audi':
-        print(f"Hello, {cars.upper()}, you are a real german car!")
-    elif cars == 'kia':
-        print(f"Hello, {cars.upper()}, you are good korean mark!")
-    else:
-        print(f"Hello, {cars.upper()}, you not bad car!")
+# cars = ['bmw', 'audi', 'kia', 'lada', 'haval']
+# for cars in cars:
+#     if cars == 'bmw':
+#         print(f"Hello, {cars.upper()}, you look very cool!")
+#     elif cars == 'audi':
+#         print(f"Hello, {cars.upper()}, you are a real german car!")
+#     elif cars == 'kia':
+#         print(f"Hello, {cars.upper()}, you are good korean mark!")
+#     else:
+#         print(f"Hello, {cars.upper()}, you not bad car!")
+
+
+
+# names = []
+# if names:
+#     for names in names:
+#         print(f"{names}")
+# else:
+#     print("We need to ind some users.")
+
+
+
