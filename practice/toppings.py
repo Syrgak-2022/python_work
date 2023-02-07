4b269621187467e12c58c8be5b4b7aadc59566ef
Syrgak-2022 <syrgakimankulov@gmail.com>
2023-02-07 11:25:06 +0300

    add new conditions in list

--- practice/toppings.py
+++ practice/toppings.py
@@ -7,10 +7,19 @@
 
 # Если в пиццерии закончится зеленый перец
 
-requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
-for requested_toppings in requested_toppings:
-    if requested_toppings == 'green peppers':
-        print("Sorry, we are out of green peppers right now.")
-    else:
+# requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
+# for requested_toppings in requested_toppings:
+#     if requested_toppings == 'green peppers':
+#         print("Sorry, we are out of green peppers right now.")
+#     else:
+#         print(f"Adding {requested_toppings}.")
+# print("\nFinished making your pizza!")
+
+
+requested_toppings = []
+if requested_toppings:
+    for requested_toppings in requested_toppings:
         print(f"Adding {requested_toppings}.")
-print("\nFinished making your pizza!")
+    print("\nFinished making your pizza!")
+else:
+    print("Are you sure you want a plain pizza?")
