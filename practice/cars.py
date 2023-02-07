75f16523a67d05e66cebf91239393359133f1268
Syrgak-2022 <syrgakimankulov@gmail.com>
2023-02-07 09:47:01 +0300

    add cars.py

---
+++ practice/cars.py
@@ -0,0 +1,68 @@
+# car = ['bmw', 'dodge', 'kia']
+# print(car)
+# car[0] = 'lada'
+# print(car)
+# car.append('bmw')
+# print(car)
+# car.insert(0, 'niva')
+# print(car)
+# del car[0]
+# print(car)
+# popped_car = car.pop()
+# print(car)
+# print(popped_car)
+# popped_car = car.pop(0)
+# print(popped_car)
+# print(car)
+# car.append('bmw')
+# print(car)
+# car.insert(0,'lada')
+# print(car)
+# del car[0]
+# print(car)
+# car.append('ural')
+# print(car)
+# car.remove('ural')
+# print(car)
+# car.remove('kia')
+# print(car)
+# car.append('kia')
+# print(car)
+# car.append('lada')
+# print(car)
+# car.append('ural')
+# car.sort()
+# print(car)
+# car.sort(reverse=True)
+# print(car)
+# print(sorted(car))
+# print(car)
+# car.reverse()
+# print(car)
+# car.reverse()
+# print(car)
+# len(car)
+# print(len(car))
+
+
+
+# cars = ['bmw', 'kia', 'lada', 'dodge', 'audi']
+# for car in cars:
+#     if car == 'bmw':
+#         print(car.upper())
+#     else:
+#         print(car.title())
+
+
+# requested_car = 'grey'
+# if requested_car != 'black':
+#     print("Hold the black")
+# else:
+#     print("Great! Grey - my favourite paint")
+
+cars = ['bmw', 'kia', 'lada', 'dodge', 'audi']
+car = 'granta'
+if car not in cars:
+    print(f"{car} not included")
+else:
+    print(f"{car} included")
