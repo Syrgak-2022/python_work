011e8898be74dbd86429d6a071bef2b5b1397027
Syrgak-2022 <syrgakimankulov@gmail.com>
2023-02-07 10:38:48 +0300

    add any versions for green, red and yellow

--- practice/practice_3_if_else.py
+++ practice/practice_3_if_else.py
@@ -1,4 +1,24 @@
+#1st version gor green
 alien_color = 'green'
+if 'green' in alien_color:
+    print("You won 5 points.")
+elif 'red' in alien_color:
+    print("You won 10 points.")
+else:
+    print("You won 15 points.")
+
+#2nd version for red
+alien_color = 'red'
+if 'green' in alien_color:
+    print("You won 5 points.")
+elif 'red' in alien_color:
+    print("You won 10 points.")
+else:
+    print("You won 15 points.")
+
+
+#3rd version for yellow
+alien_color = 'yellow'
 if 'green' in alien_color:
     print("You won 5 points.")
 elif 'red' in alien_color:
