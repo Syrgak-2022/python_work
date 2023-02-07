8ac593815d4a329ed3357e41a6e095d009f914d5
Syrgak-2022 <syrgakimankulov@gmail.com>
2023-02-07 10:37:08 +0300

    add 3rd practice_3_if_else.py

---
+++ practice/practice_3_if_else.py
@@ -0,0 +1,7 @@
+alien_color = 'green'
+if 'green' in alien_color:
+    print("You won 5 points.")
+elif 'red' in alien_color:
+    print("You won 10 points.")
+else:
+    print("You won 15 points.")
