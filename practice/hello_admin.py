5e4238497a7111ef60c3615c62a887af3db858df
Syrgak-2022 <syrgakimankulov@gmail.com>
2023-02-07 14:30:33 +0300

    add practice about cars

--- practice/hello_admin.py
+++ practice/hello_admin.py
@@ -1,6 +1,19 @@
-names = ['admin', 'syrgak', 'sancho', 'botya', 'eldo', 'tima']
-for names in names:
-    if names == 'admin':
-        print(f"Hello, admin, would you like to see a status report?")
+# names = ['admin', 'syrgak', 'sancho', 'botya', 'eldo', 'tima']
+# for names in names:
+#     if names == 'admin':
+#         print(f"Hello, admin, would you like to see a status report?")
+#     else:
+#         print(f"Hello, {names}, thank you for logging again.")
+
+
+
+cars = ['bmw', 'audi', 'kia', 'lada', 'haval']
+for cars in cars:
+    if cars == 'bmw':
+        print(f"Hello, {cars.upper()}, you look very cool!")
+    elif cars == 'audi':
+        print(f"Hello, {cars.upper()}, you are a real german car!")
+    elif cars == 'kia':
+        print(f"Hello, {cars.upper()}, you are good korean mark!")
     else:
-        print(f"Hello, {names}, thank you for logging again.")
+        print(f"Hello, {cars.upper()}, you not bad car!")
