a68739aed20ba32e983cb594b94a33991d0c1ff8
Syrgak-2022 <syrgakimankulov@gmail.com>
2023-02-07 15:26:06 +0300

    add ordinals.py

---
+++ practice/ordinals.py
@@ -0,0 +1,10 @@
+ordinals = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+for ordinals in ordinals:
+    if ordinals == 1:
+        print(f"1st")
+    elif ordinals == 2:
+        print(f"2nd")
+    elif ordinals == 3:
+        print(f"3rd")
+    else:
+        print(f"{ordinals}th")
