69f80990b12196863a9974fb1acc50f584e9c384
Syrgak-2022 <syrgakimankulov@gmail.com>
2023-02-07 14:23:59 +0300

    add hello_admin.py

---
+++ practice/hello_admin.py
@@ -0,0 +1,6 @@
+names = ['admin', 'syrgak', 'sancho', 'botya', 'eldo', 'tima']
+for names in names:
+    if names == 'admin':
+        print(f"Hello, admin, would you like to see a status report?")
+    else:
+        print(f"Hello, {names}, thank you for logging again.")
