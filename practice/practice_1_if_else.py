96bd259f63b929293ba673cfafac9afe1de4d9ea
Syrgak-2022 <syrgakimankulov@gmail.com>
2023-02-07 10:27:33 +0300

    add 2nd version program

--- practice/practice_1_if_else.py
+++ practice/practice_1_if_else.py
@@ -1,5 +1,10 @@
+# 1st version
 alien_color = 'green'
 if 'green' in alien_color:
     print("You cost 5 points")
-else:
-    print("You didn't cost points")
+
+
+# 2nd version
+alien_color = 'red'
+if 'green' in alien_color:
+    print("You cost 5 points")
