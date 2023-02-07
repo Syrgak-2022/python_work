05e18b6394b97ed5514808e9421dd884629242e3
Syrgak-2022 <syrgakimankulov@gmail.com>
2023-02-07 11:03:14 +0300

    add toppings.py

---
+++ practice/toppings.py
@@ -0,0 +1,4 @@
+requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
+for requested_toppings in requested_toppings:
+    print(f"Adding {requested_toppings}")
+print("\nFinished making you pizza!")
