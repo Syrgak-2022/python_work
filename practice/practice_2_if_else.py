000c0ea45f1fc0b332c4a7758c8e27339f6ed253
Syrgak-2022 <syrgakimankulov@gmail.com>
2023-02-07 10:32:24 +0300

    add 2nd version

--- practice/practice_2_if_else.py
+++ practice/practice_2_if_else.py
@@ -1,4 +1,13 @@
+# 1st version
 alien_color = 'green'
+if 'green' in alien_color:
+    print("You won 5 poits.")
+else:
+    print("You won 10 points")
+
+
+# 2nd version
+alien_color = 'red'
 if 'green' in alien_color:
     print("You won 5 poits.")
 else:
