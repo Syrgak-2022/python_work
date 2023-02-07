6ee5a49fca83ec6ee12cefe6a833760e0751dfa7
Syrgak-2022 <syrgakimankulov@gmail.com>
2023-02-07 10:56:09 +0300

    add practice_5_fruits

---
+++ practice/fruits.py
@@ -0,0 +1,12 @@
+my_fruits = ['apple', 'orange', 'pineapple', 'kiwi']
+print(f"My favourite fruits {my_fruits}")
+if 'apple' in my_fruits:
+    print("You really like apple!")
+if 'orange' in my_fruits:
+    print("you really like orange!")
+if 'pineapple' in my_fruits:
+    print("You really like pineapple!")
+if 'kiwi' in my_fruits:
+    print("you really like kiwi!")
+if 'melon' not in my_fruits:
+    print("You didn't like a melon!")
