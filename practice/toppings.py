a98e08647e6d2a3049020be2fb05abcad278284c
Syrgak-2022 <syrgakimankulov@gmail.com>
2023-02-07 11:32:08 +0300

    add new conditions in list (2)

--- practice/toppings.py
+++ practice/toppings.py
@@ -16,10 +16,21 @@
 # print("\nFinished making your pizza!")
 
 
-requested_toppings = []
-if requested_toppings:
-    for requested_toppings in requested_toppings:
-        print(f"Adding {requested_toppings}.")
-    print("\nFinished making your pizza!")
-else:
-    print("Are you sure you want a plain pizza?")
+# requested_toppings = []
+# if requested_toppings:
+#     for requested_toppings in requested_toppings:
+#         print(f"Adding {requested_toppings}.")
+#     print("\nFinished making your pizza!")
+# else:
+#     print("Are you sure you want a plain pizza?")
+
+
+
+available_topping = ['mushrooms', 'olives', 'green peppers', 'pepperoni', 'pineapple', 'extra cheese']
+requested_topping = ['mushrooms', 'french fries', 'extra cheese']
+for requested_topping in requested_topping:
+    if requested_topping in available_topping:
+        print(f"Adding {requested_topping}.")
+    else:
+        print(f"Sorry, we don't have {requested_topping}.")
+print("\nFinished your making pizza!")
