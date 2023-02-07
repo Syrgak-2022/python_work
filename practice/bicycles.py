17297580edbf66722cf5a8d2546b2e054a80ab3b
Syrgak-2022 <syrgakimankulov@gmail.com>
2023-02-07 09:28:02 +0300

    add bicycles.py

---
+++ practice/bicycles.py
@@ -0,0 +1,6 @@
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+# print(bicycles[2].title())
+# print(bicycles[0].upper())
+# print(bicycles[-1].title())
+message = " - is better bicycle"
+print(f"{bicycles[-1].upper()} {message}")
