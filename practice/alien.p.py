dc44ebf93cc4a85fe48c4a81178569e6793fd77c
Syrgak-2022 <syrgakimankulov@gmail.com>
2023-02-08 08:28:17 +0300

    add new tasks in alien.py

--- practice/alien.p.py
+++ practice/alien.p.py
@@ -1,8 +1,25 @@
-alien_0 = {'color': 'green', 'points': 5}
+# alien_0 = {'color': 'green', 'points': 5}
+# print(alien_0['color'])
+# print(alien_0['points'])
+#
+#
+# alien_0 = {'color': 'green', 'points': 5}
+# new_points = alien_0['points']
+# print(f"You just earned {new_points} points!")
+
+
+alien_0 = {'color': 'blue', 'points': '5', 'shape': 'circle', 'size': 'small'}
+alien_1 = {'color': 'green', 'points': '10', 'shape': 'oval', 'size': 'average'}
+alien_2 = {'color': 'red', 'point': '15', 'shape': 'square', 'size': 'big'}
+
+new_alien_0 = alien_0['color']
+new_alien_1 = alien_1['points']
+new_alien_2 = alien_2['shape']
+
 print(alien_0['color'])
-print(alien_0['points'])
+print(alien_1['shape'])
+print(alien_2['size'])
 
-
-alien_0 = {'color': 'green', 'points': 5}
-new_points = alien_0['points']
-print(f"You just earned {new_points} points!")
+print(f"You shoot down the {new_alien_0} alien!")
+print(f"Yoy've earned {new_alien_1} points!")
+print(f"You shoot down a {new_alien_2} spaceship!")
