311533b4a0842167fe338a96ed61bdf91aea73cc
Syrgak-2022 <syrgakimankulov@gmail.com>
2023-02-08 10:26:30 +0300

    add new tasks in man.py

--- practice/man.py
+++ practice/man.py
@@ -1,12 +1,31 @@
-man = {
-    'first_name': 'syrgak',
-    'last_name': 'imankulov',
-    'age': 38,
-    'city': 'nizhnekamsk',
-    'phone': +79063271448,
+# man = {
+#     'first_name': 'syrgak',
+#     'last_name': 'imankulov',
+#     'age': 38,
+#     'city': 'nizhnekamsk',
+#     'phone': +79063271448,
+#     }
+# print(man['first_name'])
+# print(man['last_name'])
+# print(man['age'])
+# print(man['city'])
+# print(man['phone'])
+
+
+favourite_numbers = {
+    'syrgak': 8,
+    'botya': 6,
+    'sancho': 4,
+    'eldo': 2,
     }
-print(man['first_name'])
-print(man['last_name'])
-print(man['age'])
-print(man['city'])
-print(man['phone'])
+numbers = favourite_numbers['syrgak']
+print(f"Syrgak's favourite number is: {numbers}")
+
+numbers = favourite_numbers['botya']
+print(f"\nBotya's favourite number is: {numbers}")
+
+numbers = favourite_numbers['sancho']
+print(f"\nSancho's favourite number is: {numbers}")
+
+numbers = favourite_numbers['eldo']
+print(f"\nEldo's favourite number is: {numbers}")
