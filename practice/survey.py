4269607e523688ca270381934b63b37062ec8752
Syrgak-2022 <syrgakimankulov@gmail.com>
2023-02-08 10:08:54 +0300

    add tasks about car in survey.py

--- practice/survey.py
+++ practice/survey.py
@@ -1,7 +1,20 @@
-favourite_languages = {
-    'syrgak': 'python',
-    'sancho': 'java',
-    'botya': 'c',
-    'eldo': 'c#',
-    'tima': 'c++',
+# favourite_languages = {
+#     'syrgak': 'python',
+#     'sancho': 'java',
+#     'botya': 'c',
+#     'eldo': 'c#',
+#     'tima': 'c++',
+#     }
+# language = favourite_languages['tima'].title()
+# print(f"Tima's favourite language is: {language}!")
+
+
+great_car = {
+    'syrgak': 'audi',
+    'botya': 'bmw',
+    'sancho': 'kia',
+    'eldo': 'dodge',
+    'tima': 'toyota'
     }
+car = great_car['syrgak'].title()
+print(f"Syrgak's favourite car is: {car}!")
