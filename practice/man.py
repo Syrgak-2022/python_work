eca2023f17fe736f4508df3f5a281becbe33646e
Syrgak-2022 <syrgakimankulov@gmail.com>
2023-02-08 10:20:32 +0300

    add man.py

---
+++ practice/man.py
@@ -0,0 +1,12 @@
+man = {
+    'first_name': 'syrgak',
+    'last_name': 'imankulov',
+    'age': 38,
+    'city': 'nizhnekamsk',
+    'phone': +79063271448,
+    }
+print(man['first_name'])
+print(man['last_name'])
+print(man['age'])
+print(man['city'])
+print(man['phone'])
