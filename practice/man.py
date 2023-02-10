dfe4a1e5c3fa9d2f7418cc8d5cda6a8594763b1a
Syrgak-2022 <syrgakimankulov@gmail.com>
2023-02-10 07:55:10 +0300

    add user_0.py

--- practice/man.py
+++ practice/man.py
@@ -12,20 +12,20 @@
 # print(man['phone'])
 
 
-favourite_numbers = {
-    'syrgak': 8,
-    'botya': 6,
-    'sancho': 4,
-    'eldo': 2,
-    }
-numbers = favourite_numbers['syrgak']
-print(f"Syrgak's favourite number is: {numbers}")
-
-numbers = favourite_numbers['botya']
-print(f"\nBotya's favourite number is: {numbers}")
-
-numbers = favourite_numbers['sancho']
-print(f"\nSancho's favourite number is: {numbers}")
-
-numbers = favourite_numbers['eldo']
-print(f"\nEldo's favourite number is: {numbers}")
+# favourite_numbers = {
+#     'syrgak': 8,
+#     'botya': 6,
+#     'sancho': 4,
+#     'eldo': 2,
+#     }
+# numbers = favourite_numbers['syrgak']
+# print(f"Syrgak's favourite number is: {numbers}")
+#
+# numbers = favourite_numbers['botya']
+# print(f"\nBotya's favourite number is: {numbers}")
+#
+# numbers = favourite_numbers['sancho']
+# print(f"\nSancho's favourite number is: {numbers}")
+#
+# numbers = favourite_numbers['eldo']
+# print(f"\nEldo's favourite number is: {numbers}")
---
+++ practice/user_0.py
@@ -0,0 +1,8 @@
+user_0 = {
+    'username': 'simankulov',
+    'first_name': 'syrgak',
+    'lastname': 'imankulov',
+    }
+for key, value in user_0.items():
+    print(f"\nKey: {key}")
+    print(f"Value: {value}")
