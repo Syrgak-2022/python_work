fe229b548f2e1c37a7ccd7566e0e9e3981e0feec
Syrgak-2022 <syrgakimankulov@gmail.com>
2023-02-10 08:02:25 +0300

    add new tasks in user_0.py

--- practice/user_0.py
+++ practice/user_0.py
@@ -1,8 +1,34 @@
-user_0 = {
-    'username': 'simankulov',
-    'first_name': 'syrgak',
-    'lastname': 'imankulov',
+# user_0 = {
+#     'username': 'simankulov',
+#     'first_name': 'syrgak',
+#     'lastname': 'imankulov',
+#     }
+# for key, value in user_0.items():
+#     print(f"\nKey: {key}")
+#     print(f"Value: {value}")
+
+
+
+# car = {
+#     'model': 'bmw',
+#     'year': 2022,
+#     'paint': 'black',
+#     'all_weels': 'full'
+#     }
+# for key, value in car.items():
+#     print(f"\nKey: {key}")
+#     print(f"Value: {value}")
+
+
+
+
+console = {
+    'name': 'xbox',
+    'year': 2023,
+    'paint': 'black',
+    'gamepass': '12 months',
+    'guarantee': 'full',
     }
-for key, value in user_0.items():
+for key, value in console.items():
     print(f"\nKey: {key}")
     print(f"Value: {value}")
