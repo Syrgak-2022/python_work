8b449c2793c6123e57d70618bb06d5a5b49964e4
Syrgak-2022 <syrgakimankulov@gmail.com>
2023-02-10 08:47:19 +0300

    add new tasks in user_0.py

--- practice/user_0.py
+++ practice/user_0.py
@@ -36,21 +36,56 @@
 
 
 
-console = {
-    'name': 'xbox',
-    'year': 2023,
-    'paint': 'black',
-    'gamepass': '12 months',
-    'guarantee': 'full',
-    }
-for key in console.keys():
-    print(key)
+# console = {
+#     'name': 'xbox',
+#     'year': 2023,
+#     'paint': 'black',
+#     'gamepass': '12 months',
+#     'guarantee': 'full',
+#     }
+# for key in console.keys():
+#     print(key)
+#
+# car = {
+#     'model': 'bmw',
+#     'year': 2022,
+#     'paint': 'black',
+#     'all_weels': 'full'
+#     }
+# for key in car.keys():
+#     print(key)
 
-car = {
-    'model': 'bmw',
-    'year': 2022,
-    'paint': 'black',
-    'all_weels': 'full'
-    }
-for key in car.keys():
-    print(key)
+
+# favourite_languages = {
+#     'syrgak': 'python',
+#     'botya': 'c++',
+#     'sancho': 'java',
+#     'eldo': 'c#',
+#     'tima': 'dolphin'
+#     }
+# friends = ['syrgak', 'botya']
+# for name in favourite_languages.keys():
+#     print(name.title())
+#     if name in friends:
+#         language = favourite_languages[name].title()
+#         print(f"\t{name.title()}, I see you love {language}")
+
+
+
+
+# favourite_cars = {
+#     'syrgak': 'dodge',
+#     'botya': 'bmw',
+#     'sancho': 'audi',
+#     'eldo': 'kia',
+#     'tima': 'haval',
+#     }
+# names = ['eldo', 'tima']
+# for name in favourite_cars.keys():
+#     print(name.title())
+#     if name in names:
+#         car = favourite_cars[name]
+#         print(f"\t{name.title()}, I see you love {car}")
+
+
+
