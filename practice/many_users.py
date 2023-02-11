244bce5bf03c155f8cabf2547be04c60f50b909b
Syrgak-2022 <syrgakimankulov@gmail.com>
2023-02-11 12:34:43 +0300

    add new tasks (cars) in many_users.py

--- practice/many_users.py
+++ practice/many_users.py
@@ -19,25 +19,47 @@
 
 
 
-brothers = {
-    'isancho': {
-        'first': 'sancho',
-        'last': 'imankulov',
-        'location': 'bishkek',
+# brothers = {
+#     'isancho': {
+#         'first': 'sancho',
+#         'last': 'imankulov',
+#         'location': 'bishkek',
+#     },
+#     'botyaim': {
+#         'first': 'botya',
+#         'last': 'imankulov',
+#         'location': 'bishkek',
+#     },
+# }
+# for user_name, user_info in brothers.items():
+#     print(f"Username: {user_name}")
+#     fullname = f"{user_info['first']} {user_info['last']}"
+#     location = user_info['location']
+#     print(f"\tFull name: {fullname.title()}")
+#     print(f"\tLocation: {location.title()}")
+
+
+
+
+
+cars = {
+    'domestic': {
+        'name': 'lada',
+        'model': 'granta',
+        'location': 'russia',
     },
-    'botyaim': {
-        'first': 'botya',
-        'last': 'imankulov',
-        'location': 'bishkek',
+    'foreign': {
+        'name': 'dodge',
+        'model': 'caliber',
+        'location': 'usa',
     },
 }
-for user_name, user_info in brothers.items():
-    print(f"Username: {user_name}")
-    fullname = f"{user_info['first']} {user_info['last']}"
-    location = user_info['location']
-    print(f"\tFull name: {fullname.title()}")
-    print(f"\tLocation: {location.title()}")
-
+for manufacture, cars_info in cars.items():
+    print(f"Manufacture: {manufacture.title()}")
+    carsinformation = f"{cars_info['name']} {cars_info['model']}"
+    carslocation = cars_info['location']
+    print(f"\tFull name: {carsinformation.title()}")
+    print(f"\tLocation: {carslocation.upper()}")
 
 
 
