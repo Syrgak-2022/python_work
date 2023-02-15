92117103f408126d714dd32061865c4a8e46ca51
Syrgak-2022 <syrgakimankulov@gmail.com>
2023-02-15 09:35:06 +0300

    add new tasks in even_or_odd.py

--- practice/practice_input.py
+++ practice/practice_input.py
@@ -2,8 +2,16 @@
 # print(f"\nLet me see if I can find you a {car.title()}")
 
 
-order = input("How many seats do you want to book? ")
-if order >= '8':
-    print(f"Please wait!")
+# order = input("How many seats do you want to book? ")
+# if order >= '8':
+#     print(f"Please wait!")
+# else:
+#     print(f"\nYour seats are ready!")
+
+
+number = input("Enter your number: ")
+number = int(number)
+if number % 10 == 0:
+    print("\nYour number is multiple!")
 else:
-    print(f"\nYour seats are ready!")
+    print("\nYour number is not multiple!")
