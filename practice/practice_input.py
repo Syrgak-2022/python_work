ae7faae780b7adc1c80ef059923f267f01881e86
Syrgak-2022 <syrgakimankulov@gmail.com>
2023-02-15 09:14:49 +0300

    add task in even_or_odd.py

---
+++ practice/practice_input.py
@@ -0,0 +1,9 @@
+# car = input("What car do you want to a buy? ")
+# print(f"\nLet me see if I can find you a {car.title()}")
+
+
+order = input("How many seats do you want to book? ")
+if order >= '8':
+    print(f"Please wait!")
+else:
+    print(f"\nYour seats are ready!")
