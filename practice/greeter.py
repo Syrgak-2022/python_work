c2564acb0df283c8b4e5cb177eaa28306106491a
Syrgak-2022 <syrgakimankulov@gmail.com>
2023-02-15 08:36:47 +0300

    add new task greeter.py

--- practice/greeter.py
+++ practice/greeter.py
@@ -1,2 +1,7 @@
-name = input("Please, enter your name: ")
-print(f"\nHello, {name.title()}")
+# name = input("Please, enter your name: ")
+# print(f"\nHello, {name.title()}")
+#
+# prompt = "If you tell us who are you, we can personalize the messages you see."
+# prompt += "\nWhat is your first name? "
+# name = input(prompt)
+# print(f"\nHello, {name}")
