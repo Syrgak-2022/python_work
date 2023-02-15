4e42aad06c12e9da230ae52af7e850fbef6fd8fd
Syrgak-2022 <syrgakimankulov@gmail.com>
2023-02-15 10:14:28 +0300

    add new any tasks in parrot.py

--- practice/parrot.py
+++ practice/parrot.py
@@ -5,17 +5,25 @@
 #     message = input(prompt)
 #     print(message)
 
-# bit = "Tell me something, and I will repeat it back to you: "
-# bit += "\nEnter 'quit' to end the programm. "
-# message = ""
-# while message != 'quit':
-#     message = input(bit)
-#     print(message)
+# # bit = "Tell me something, and I will repeat it back to you: "
+# # bit += "\nEnter 'quit' to end the programm. "
+# # message = ""
+# # while message != 'quit':
+# #     message = input(bit)
+# #     print(message)
+#
+#
+# cart = "Please, check your armour, before you leave this store"
+# cart += "\nIf you leave this store without water, please, check safe-box!"
+# cart += "\nIf you leave this store without food, plaese, check live-box"
+# cart += "\nPlease, enter your name: "
+# name = input(cart)
+# print(name.title())
 
 
-cart = "Please, check your armour, before you leave this store"
-cart += "\nIf you leave this store without water, please, check safe-box!"
-cart += "\nIf you leave this store without food, plaese, check live-box"
-cart += "\nPlease, enter your name: "
-name = input(cart)
-print(name.title())
+car = "Please, check your order in our store!"
+car += "\nPlease, enter 'exit' to quit from this programm! "
+message = ""
+while message != 'exit':
+    message = input(car)
+    print(message)
