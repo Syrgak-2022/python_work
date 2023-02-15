2160e92ede629f9b6e25f303a87aafc227746de5
Syrgak-2022 <syrgakimankulov@gmail.com>
2023-02-15 10:03:34 +0300

    add new tasks in parrot.py

--- practice/parrot.py
+++ practice/parrot.py
@@ -1,6 +1,21 @@
-prompt = "Tell me something, and I will repeat it back to you: "
-prompt += "\nEnter 'quit' to end the programm. "
-message = ""
-while message != 'quit':
-    message = input(prompt)
-    print(message)
+# prompt = "Tell me something, and I will repeat it back to you: "
+# prompt += "\nEnter 'quit' to end the programm. "
+# message = ""
+# while message != 'quit':
+#     message = input(prompt)
+#     print(message)
+
+# bit = "Tell me something, and I will repeat it back to you: "
+# bit += "\nEnter 'quit' to end the programm. "
+# message = ""
+# while message != 'quit':
+#     message = input(bit)
+#     print(message)
+
+
+cart = "Please, check your armour, before you leave this store"
+cart += "\nIf you leave this store without water, please, check safe-box!"
+cart += "\nIf you leave this store without food, plaese, check live-box"
+cart += "\nPlease, enter your name: "
+name = input(cart)
+print(name.title())
