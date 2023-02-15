f6819940599bbf60f8293529be4eb00dd83c027e
Syrgak-2022 <syrgakimankulov@gmail.com>
2023-02-15 09:51:22 +0300

    add new task in parrot.py

--- practice/parrot.py
+++ practice/parrot.py
@@ -1,2 +1,6 @@
-massage = input("Tell me something, and I will repeat it back to you: ")
-print(massage)
+prompt = "Tell me something, and I will repeat it back to you: "
+prompt += "\nEnter 'quit' to end the programm. "
+message = ""
+while message != 'quit':
+    message = input(prompt)
+    print(message)
