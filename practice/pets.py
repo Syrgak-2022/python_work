1a76d00a6321359a30584070e3d64a24c19aa2be
Syrgak-2022 <syrgakimankulov@gmail.com>
2023-02-17 14:53:22 +0300

    add pets.py

---
+++ practice/pets.py
@@ -0,0 +1,23 @@
+# pets = ['dog', 'cat', 'dog', 'goldfish', 'cat', 'rabbit', 'cat']
+# print(pets)
+#
+# while 'cat' in pets:
+#     pets.remove('cat')
+# print(pets)
+
+
+
+# names = ['syrgak', 'eldo', 'tima', 'syrgak', 'aselya', 'sancho', 'syrgak', 'botya', 'sezima', 'syrgak', 'arushka', 'syrgak']
+# print(names)
+#
+# while 'syrgak' in names:
+#     names.remove('syrgak')
+# print(names)
+
+
+cars = ['bmw', 'kia', 'lada', 'bmw', 'dodge', 'bmw', 'haval', 'honda']
+print(cars)
+
+while 'bmw' in cars:
+    cars.remove('bmw')
+print(cars)
