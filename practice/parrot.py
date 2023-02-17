23e1ee23c7b26f647c5c3c6db06295cec2d32fe1
Syrgak-2022 <syrgakimankulov@gmail.com>
2023-02-17 10:05:23 +0300

    add new many any tasks in parrot.py

--- practice/parrot.py
+++ practice/parrot.py
@@ -21,9 +21,49 @@
 # print(name.title())
 
 
-car = "Please, check your order in our store!"
-car += "\nPlease, enter 'exit' to quit from this programm! "
-message = ""
-while message != 'exit':
-    message = input(car)
-    print(message)
+# car = "Please, check your order in our store!"
+# car += "\nPlease, enter 'exit' to quit from this programm! "
+# message = ""
+# while message != 'exit':
+#     message = input(car)
+#     print(message)
+
+
+# message = input("Tell me something, nd I'll repeat it back to you: ")
+# print(message)
+
+# name = input("Please, enter you name: ")
+# print(f"\n Hello, {name.title()}")
+
+# prompt = "If you tell us who are you, we can personalizethe messages you see."
+# prompt += "\nWhat is your first name? "
+# name = input(prompt)
+# print(f"\nHello, {name.title()}")
+
+# age = input("How old are you? ")
+# print(age)
+
+# height = input("How tall are you, in inches? ")
+# height = int(height)
+# if height >= 48:
+#     print("\nYou are tall enough to ride!")
+# else:
+#     print("\nYou will ba able to ride when ou are a little order.")
+
+
+# age = input("How old are you for this park? ")
+# age = int(age)
+# if age >= 20:
+#     print("\nAccess granted!")
+# else:
+#     print("\nSorry, access denied!")
+
+
+# number = input("Enter a number, and I will tell you if it is even or odd: ")
+# number = int(number)
+# if number % 2 == 0:
+#     print(f"\nThis number {number} is even.")
+# else:
+#     print(f"\nThis number {number} is odd.")
+
+
