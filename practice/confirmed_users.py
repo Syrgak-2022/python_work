ec8e094230d9053d65f4788103f7ec7d7f4b41c1
Syrgak-2022 <syrgakimankulov@gmail.com>
2023-02-17 14:46:33 +0300

    add confirmed_users.py

---
+++ practice/confirmed_users.py
@@ -0,0 +1,42 @@
+# unconfirmed_users = ['sancho', 'botya', 'syrgak']
+# confirmed_users = []
+#
+# while unconfirmed_users:
+#     current_user = unconfirmed_users.pop()
+#
+#     print(f"Verifying user: {current_user.title()}")
+#     confirmed_users.append(current_user.title())
+#
+# print("\nThe following users have been confirmed:")
+# for confirmed_users in confirmed_users:
+#     print(confirmed_users.title())
+#
+#
+#
+# unregistered_cars = ['bmw', 'kia', 'honda', 'lada']
+# registered_cars = []
+#
+# while unregistered_cars:
+#     current_cars = unregistered_cars.pop()
+#
+#     print(f"Verifying cars: {current_cars.title()}")
+#     registered_cars.append(current_cars.title())
+#
+# print(f"The following cars have been confirmed: ")
+# for registered_cars in registered_cars:
+#     print(registered_cars.title())
+
+
+
+unconfirmed_games = ['halo', 'far cry', 'gears', 'tekken']
+confirmed_games = []
+
+while unconfirmed_games:
+    current_games = unconfirmed_games.pop()
+
+    print(f"Verifying games: {current_games}")
+    confirmed_games.append(current_games)
+
+print("\nFollowing games have been confirmed: ")
+for confirmed_games in confirmed_games:
+    print(confirmed_games)
