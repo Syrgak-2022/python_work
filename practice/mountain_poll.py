42d55c17d2aa22e991bdb6d734447155f319ea58
Syrgak-2022 <syrgakimankulov@gmail.com>
2023-02-17 15:17:09 +0300

    add tasks in mountain_poll.py

--- practice/mountain_poll.py
+++ practice/mountain_poll.py
@@ -1,17 +1,35 @@
+# responses = {}
+#
+# polling_active = True
+#
+# while polling_active:
+#     name = input("\nWhat is our name? ")
+#     response = input("Which mountain would you like to climb someday? ")
+#
+#     responses[name] = response
+#
+#     repeat = input("Would you like to let another person respon? (yes/no) ")
+#     if repeat == 'no':
+#         polling_active = False
+#
+# print("\n---Poll Result---")
+# for name, response in responses.items():
+#     print(f"{name} would like climb {response}.")
+
+
 responses = {}
 
 polling_active = True
 
 while polling_active:
-    name = input("\nWhat is our name? ")
-    response = input("Which mountain would you like to climb someday? ")
+    name = input("What is your name? ")
+    response = input("What car is favourite for you? ")
 
     responses[name] = response
 
-    repeat = input("Would you like to let another person respon? (yes/no) ")
+    repeat = input("whould you like to let another person respon? (yes/no) ")
     if repeat == 'no':
         polling_active = False
-
-print("\n---Poll Result---")
+print("---Poll Results---")
 for name, response in responses.items():
-    print(f"{name} would like climb {response}.")
+    print(f"{name.title()} like {response.title()}")
