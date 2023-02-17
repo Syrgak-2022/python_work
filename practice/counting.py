c2d70ede1680e091ac9fa17db213c44c8f20f059
Syrgak-2022 <syrgakimankulov@gmail.com>
2023-02-17 10:59:35 +0300

    add new tasks in cities.py

--- practice/counting.py
+++ practice/counting.py
@@ -1,4 +1,22 @@
-current_number = 2
-while current_number <= 5:
-    print(current_number)
-    current_number +=1
+# current_number = 2
+# while current_number <= 5:
+#     print(current_number)
+#     current_number +=1
+
+
+# current_number = 0
+# while current_number < 10:
+#     current_number += 1
+#     if current_number % 2 == 0:
+#         continue
+#     print(current_number)
+
+# current_numbers = 1
+# while current_numbers < 20:
+#     print(current_numbers)
+#     current_numbers += 2
+
+x = 1
+while x < 5:
+    print(x)
+    x += 1
